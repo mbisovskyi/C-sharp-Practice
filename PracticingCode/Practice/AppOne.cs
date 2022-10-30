957be4c1bcf93c224976351da908e0a63cd90c61
MBisovskyi <mbisovskyi@gmail.com>
2022-10-30 09:50:49 -0400

    Feature: Created a method that returns a string of a cleared phone number from "-" if it contains it.Initial commit

--- PracticingCode/Practice/AppOne.cs
+++ PracticingCode/Practice/AppOne.cs
@@ -6,6 +6,18 @@ namespace Practice
 {
     internal class AppOne
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your phone number: ");
+            string phoneNumberInput = Console.ReadLine();
+            Console.WriteLine(HidePhoneNumber(phoneNumberInput));
+        }
 
+        static string HidePhoneNumber(string phoneNumber)
+        {
+            string[] separatingSymbols = new string[] { "/", "-", ".", " ", ",", "*" };
+            phoneNumber = phoneNumber.Replace(separatingSymbols[1], "");
+            return phoneNumber;
+        }
     }
 }
